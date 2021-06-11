8e272941c27b2dacb936ba45f5519736560bbbe8
bharathireddicherla <68938765+bharathireddicherla@users.noreply.github.com>
2021-06-11 17:34:12 +0530

    Create newfile.tf

---
+++ newfile.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 2.0"
+    }
+}
+}
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "bharathibucket" {
+  bucket =  "bharathibucket"
+}
